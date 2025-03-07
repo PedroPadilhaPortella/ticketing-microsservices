aa3b02543c7898d34bc8ac2f7cace0bdce68a914
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2025-03-06 22:49:21 -0300

    sharing code between services

---
+++ common/src/index.ts
@@ -0,0 +1,3 @@
+export * from './middlewares';
+export * from './events';
+export * from './errors';
---
+++ common/src/middlewares/index.ts
@@ -0,0 +1,4 @@
+export * from './validate-request';
+export * from './error-handler';
+export * from './require-auth';
+export * from './current-user';

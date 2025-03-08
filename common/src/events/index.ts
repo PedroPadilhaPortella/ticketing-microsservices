9f6e3b3edd4a7847384d9e587dd467baa84d23a6
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2025-03-08 16:40:06 -0300

    create order events into common package

--- common/src/events/index.ts
+++ common/src/events/index.ts
@@ -3,5 +3,7 @@ export * from './publisher';
 export * from './subjects';
 export * from './ticket-created.event';
 export * from './ticket-updated.event';
+export * from './order-created.event';
+export * from './order-cancelled.event';
 
 export * from './types/order-status';
